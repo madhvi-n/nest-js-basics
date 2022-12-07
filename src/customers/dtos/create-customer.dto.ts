3b1812b53c855157ee9a7835964399b008613e33
madhvi-n <madhvinandanwar@gmail.com>
2022-12-07 22:05:17 +0530

    commit: user controller

--- src/customers/dtos/create-customer.dto.ts
+++ src/customers/dtos/create-customer.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsNumberString, ValidateNested } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsNumberString, IsNotEmptyObject, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
 import { CreateAddressDto } from './create-address.dto';
 
--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor, Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Inject,
+  Param,
+  UseInterceptors
+} from '@nestjs/common';
+import { UsersService } from 'src/users/services/users/users.service';
+import { SerializedUser } from 'src/users/types/user';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(@Inject('USER_SERVICE') private readonly userService: UsersService){
+
+  }
+
+  @Get('')
+  getUsers() {
+    this.userService.getUsers();
+  }
+
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Get('/:username')
+  getUserByUsername(@Param('username') username: string){
+    const user =  this.userService.getUserByUsername(username);
+    if (user) return new SerializedUser(user);
+    else throw new HttpException('User not found', HttpStatus.BAD_REQUEST);
+  }
+}
