610a479259b45e022e70cce8cd8bf43a5f1c138a
madhvi-n <madhvinandanwar@gmail.com>
2022-12-08 16:18:38 +0530

    commit: postgres typeorm connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,27 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomersModule } from './customers/customers.module';
-import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
+import { ProductsModule } from './products/products.module';
+import entities from './typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     UsersModule,
     CustomersModule,
     ProductsModule,
-    OrdersModule
+    OrdersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'nest_db',
+      entities: entities,
+      synchronize: true
+    }),
   ],
   controllers: [],
   providers: [],
